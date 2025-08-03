e38e3aa46833daf1ca081b2bfee9d20bd855b274
Veeral28 <122989144+Veeral28@users.noreply.github.com>
2025-08-04 04:34:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -63,3 +63,6 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/intern/index.html');
+});
